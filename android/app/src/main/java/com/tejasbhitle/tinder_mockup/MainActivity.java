ae6217b23d85ebbef9846b1d1eb8e250ea67bc73
TejasBhitle <bhitletejas@gmail.com>
2019-06-10 11:30:39 +0530

    Initial commit

---
+++ android/app/src/main/java/com/tejasbhitle/tinder_mockup/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tejasbhitle.tinder_mockup;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
